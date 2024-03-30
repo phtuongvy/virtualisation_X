d6c86dc47d0e7bfc1246adc47e12b691a83bb6ac
phtuongvy <Tuong-Vy.Pham-Huynh@etu.univ-smb.fr>
2024-03-30 16:47:30 +0100

    modif index.js with login and register path

--- docker_V.6/docker/backend/index.js
+++ docker_V.6/docker/backend/index.js
@@ -28,6 +28,64 @@ var connectionOptions = {
    res.sendFile(__dirname + '/index.html');
  });
 
+ app.post('/login', function(req, res) {
+      
+      var connection = mysql.createConnection(connectionOptions);
+      var queryStr = 'SELECT * FROM `YUSER` WHERE `YUSERNAME` = ? AND `YPASSWORD` = ?';
+      
+      connection.connect();
+   
+      connection.query(queryStr, [req.query.username, req.query.password], function (error, results, fields) {
+      
+         if (error) {
+         console.error('Une erreur est survenue lors de la requête à la base de données:', error);
+         res.status(500).json({error: "Une erreur interne est survenue"});
+         return;
+         }
+      
+      if(results.length == 0) {
+         res.status(404).json({message: "Nom d'utilisateur ou mot de passe incorrect"});
+         return;
+      }
+   
+      res.status(200).json(results);
+      });
+      
+      connection.end();
+ });
+
+ var bcrypt = require('bcrypt');
+ var saltRounds = 10;
+ 
+ app.post('/register', function(req, res) {
+     var connection = mysql.createConnection(connectionOptions);
+     var queryStr = 'INSERT INTO `YUSER` (`YUSERID`, `YUSERPSEUDO`, `YUSERNAME`, `YUSERBIRTHDAY`, `YUSERPASSWORD`, `YUSERROLE`, `YUSERPREMIUM`) VALUES (?, ?, ?, ?, ?, ?, ?)';
+ 
+     // Hash password
+     bcrypt.hash(req.body.YUSERPASSWORD, saltRounds, function(err, hash) {
+         if (err) {
+            console.error('Une erreur est survenue lors du hachage du mot de passe:', err);
+             res.status(500).json({error: "Une erreur interne est survenue"});
+             return;
+         }
+ 
+         connection.connect();
+ 
+         connection.query(queryStr, [req.body.YUSERID, req.body.YUSERPSEUDO, req.body.YUSERNAME, req.body.YUSERBIRTHDAY, hash, req.body.YUSERROLE, req.body.YUSERPREMIUM], function (error, results, fields) {
+             if (error) {
+                console.error('Une erreur est survenue lors de la requête à la base de données:', error);
+                 res.status(500).json({error: "Une erreur interne est survenue"});
+                 return;
+             }
+ 
+             res.status(200).json({message: "Utilisateur enregistré avec succès"});
+         });
+ 
+         connection.end();
+     });
+ });
+
+
  app.get('/comment', function(req, res) {
    
    var connection = mysql.createConnection(connectionOptions);
