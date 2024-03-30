be730a9988622cf5c96a4e47464ad8b3e184bfb9
phtuongvy <Tuong-Vy.Pham-Huynh@etu.univ-smb.fr>
2024-03-30 23:17:45 +0100

    remove unnecessary CORS configuration

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,6 @@ var app = express();
 
 // for CORS error
 
-app.all('*', function(req, res, next) {
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Credentials', true);
-   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-   res.header('Access-Control-Allow-Headers', 'Origin, Accept, X-Requested-With, Content-Type');
-   next();
-});
-
 app.all('', function(req, res, next) {
    res.header('Access-Control-Allow-Origin', '');
    res.header('Access-Control-Allow-Credentials', true);
